a3638b14a7d2e88c5d7af8ee8c3ce0d286a36603
yojin102 <121215616+yojin102@users.noreply.github.com>
2023-12-12 01:16:37 +0900

    Add files via upload

---
+++ e1125.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex1125
+{
+    internal class e1125
+    {
+        static void Main(string[] args)
+        {
+            List<int> selectedNumbers = GenerateRandomNumbers();
+
+            // 선택된 숫자 출력
+            Console.WriteLine("선택된 숫자:");
+            foreach (int number in selectedNumbers)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        static List<int> GenerateRandomNumbers()
+        {
+            List<int> numbers = new List<int>();
+            Random random = new Random();
+            int minValue = 1;
+            int maxValue = 45;
+
+            while (numbers.Count < 6)
+            {
+                int randomNumber = random.Next(minValue, maxValue + 1);
+
+                if (!numbers.Contains(randomNumber))
+                {
+                    numbers.Add(randomNumber);
+                }
+            }
+
+            return numbers;
+        }
+    }
+    
+}
